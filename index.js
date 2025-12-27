522922021a8ece40684f3d594f253dc794ce48cf
yashi-15 <sharmayashi270@gmail.com>
2025-12-27 18:32:02 +0530

    added module exports app

--- index.js
+++ index.js
@@ -37,5 +37,4 @@ app.get("/", (req, res) => {
     res.send("Capita Care Backend is running 🚀");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+module.exports = app;
